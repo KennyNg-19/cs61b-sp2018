c18bbe1d5ccad14d74092a18379bd79b61d7c1f2
Yuan Liang <yuanbaba@ryougens-MacBook-puro.local>
2018-10-17 19:18:55 -0400

    finish

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -9,35 +9,35 @@ public class TestArrayDequeGold {
     private int loopVal = 10;
 
     /* This is for task 1, which we have found that error appears in the removeLast method.*/
-//    @Test
-//    public void testDeque() {
-//        for (int i = 0; i <= 10; i++) {
-//            int first = StdRandom.uniform(10);
-//            student.addFirst(first);
-//            solution.addFirst(first);
-//        }
-//        //student.printDeque();
-//        //solution.printDeque();
-//        for (int i = 0; i <= 10; i++) {
-//            assertEquals(student.removeFirst(), solution.removeFirst());
-//        }
-//        for (int i = 0; i <= 10; i++) {
-//            int last = StdRandom.uniform(11, 20);
-//            student.addLast(last);
-//            solution.addLast(last);
-//        }
-//        //student.printDeque();
-//        //solution.printDeque();
-//        for (int i = 0; i <= 10; i++) {
-//            assertEquals(student.removeLast(), solution.removeLast());
-//        }
-//    }
-//    public static void main(String[] args) {
-//        for (int i = 0; i < 10; i++) {
-//            Integer first = StdRandom.uniform(10);
-//            System.out.println(first);
-//        }
-//    }
+    @Test
+    public void testDeque() {
+        for (int i = 0; i <= 10; i++) {
+            int first = StdRandom.uniform(10);
+            student.addFirst(first);
+            solution.addFirst(first);
+        }
+        //student.printDeque();
+        //solution.printDeque();
+        for (int i = 0; i <= 10; i++) {
+            assertEquals(student.removeFirst(), solution.removeFirst());
+        }
+        for (int i = 0; i <= 10; i++) {
+            int last = StdRandom.uniform(11, 20);
+            student.addLast(last);
+            solution.addLast(last);
+        }
+        //student.printDeque();
+        //solution.printDeque();
+        for (int i = 0; i <= 10; i++) {
+            assertEquals(student.removeLast(), solution.removeLast());
+        }
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Integer first = StdRandom.uniform(10);
+            System.out.println(first);
+        }
+    }
 
     /* This is for task 2*/
     @Test
