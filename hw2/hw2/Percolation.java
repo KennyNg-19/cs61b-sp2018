6d86f21f6a1b81a120321a00c2ce4376a1d7e17a
Yuan Liang <yuanbaba@ryougens-MacBook-puro.local>
2018-11-13 21:20:30 -0500

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,102 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-
 public class Percolation {
+    /** define necessary attribute */
+    private int size = 0;
+    private int dim;
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF ufWithoutBottom;
+    private boolean[][] openState;
+    /** transform (x, y) into a single number */
+    private int xyToNum(int x, int y, int N){
+        return x * N + y + 1;
+    }
+    /** judge if the adjacent site is open and make union if possible*/
+    private void unionNeighbour(int row, int col, int adjacentR, int adjacentC, int dim) {
+        if ((adjacentR < dim && adjacentR >= 0) && (adjacentC < dim && adjacentC >= 0)) {
+            if (isOpen(adjacentR, adjacentC)) {
+                int idx = xyToNum(row, col, dim);
+                int adjacentIdx = xyToNum(adjacentR, adjacentC, dim);
+                uf.union(idx, adjacentIdx);
+                ufWithoutBottom.union(idx, adjacentIdx);
+            }
+        }
+    }
+
+    /** constructor of the percolation*/
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IndexOutOfBoundsException("The value of N should be greater than zero");
+        }
+        dim = N;
+        uf = new WeightedQuickUnionUF(N * N + 2); // consider two extra node representing top and bottom node.
+        ufWithoutBottom = new WeightedQuickUnionUF(N * N + 2);
+        openState = new boolean[N][N];
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                openState[i][j] = false;
+            }
+        }
+    }
+
+    /** implement open method */
+    public void open(int row, int col) {
+        if (row > dim || col > dim) {
+            throw new java.lang.IndexOutOfBoundsException("the index is going beyond the boundary");
+        }
+        openState[row][col] = true;
+        int index = xyToNum(row, col, dim);
+        if (row == 0) {
+            uf.union(0, index);
+            ufWithoutBottom.union(0, index);
+//            System.out.println("the site is connected with water");
+        } else if (row == dim - 1) {
+            uf.union(dim * dim + 1, index);
+        }
+        unionNeighbour(row, col, row - 1, col, dim);
+        unionNeighbour(row, col, row + 1, col, dim);
+        unionNeighbour(row, col, row, col - 1, dim);
+        unionNeighbour(row, col, row, col + 1, dim);
+        size += 1;
+    }
+
+    /** judge whether the (row, col) is open*/
+    public boolean isOpen(int row, int col){
+        return openState[row][col];
+    }
+
+
+    /** judge whether the (row, col) is full */
+    public boolean isFull(int row, int col) {
+        int index = xyToNum(row, col, dim);
+        return ufWithoutBottom.connected(0, index);
+
+    }
+
+    /** count the number of open sites */
+    public int numberOfOpenSites() {
+        return size;
+    }
+
+    /** judge whether the system is percolate*/
+    public boolean percolates() {
+        return uf.connected(0, dim * dim + 1);
+    }
+
+    public static void main(String[] args) {
+        Percolation per = new Percolation(10);
+        per.open(0, 1);
+        per.open(1, 1);
+        System.out.println(per.isFull(1, 1));
+        per.open(0, 2);
+        per.open(0, 3);
+//        System.out.println(per.numberOfOpenSites());
+        System.out.println(per.isFull(0, 2));
+        System.out.println(per.isOpen(0, 1));
+        System.out.println(per.isOpen(0, 2));
+
+
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,51 @@
 package hw2;
-
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+import java.lang.Math;
 public class PercolationStats {
+    private int[] result;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException("N and T should be a positive integer");
+        }
+        result = new int[T];
+        for (int i = 0; i < N; i++) {
+            Percolation per;
+            per = pf.make(N);
+            while (! per.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                if (! per.isOpen(row, col)) {
+                    per.open(row, col);
+                }
+                if (per.percolates()) {
+                    break;
+                }
+            }
+            result[i] = per.numberOfOpenSites();
+        }
 
+    }
+
+    public double mean() {
+        return StdStats.mean(result);
+
+    }
+
+    public double stddev() {
+        return StdStats.stddev(result);
+
+    }
+
+    public double confidenceLow() {
+        double meanVal = mean();
+        double devVal = stddev();
+        return meanVal - 1.96 * devVal / Math.sqrt(result.length);
+    }
+
+    public double confidenceHigh() {
+        double meanVal = mean();
+        double devVal = stddev();
+        return meanVal + 1.96 * devVal / Math.sqrt(result.length);
+    }
 }
