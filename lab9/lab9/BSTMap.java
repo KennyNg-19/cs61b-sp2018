ed652eb52ccf9075cbfe337778814c5780341180
Yuan Liang <yuanbaba@ryougens-MacBook-puro.local>
2018-11-19 14:54:50 -0500

    complete BST implementation

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -96,19 +97,83 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
 
+    private Set<K> keySetHelper(Node root) {
+        if (root == null) {
+            return new HashSet<>();
+        } else {
+            HashSet<K> set = new HashSet<>();
+            set.add(root.key);
+            set.addAll(keySetHelper(root.left));
+            set.addAll(keySetHelper(root.right));
+            return set;
+        }
+    }
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        return keySetHelper(root);
     }
 
+
+
+    private Node min(Node root) {
+        if (root.left == null) {
+            return root;
+        } else {
+            return min(root.left);
+        }
+    }
+
+    private Node removeMin(Node root) {
+        if (root.left == null) {
+            return root.right; //the minimum has removed and only the right node remove;
+        } else {
+            root.left = removeMin(root.left);
+        }
+        return root;
+    }
+
+    private Node removeHelper(K key, Node root) {
+        if (root == null) {
+            return null;
+        } else {
+            int cmp = key.compareTo(root.key);
+            if (cmp < 0) {
+                root.left = removeHelper(key, root.left);
+            } else if (cmp > 0) {
+                root.right = removeHelper(key, root.right);
+            } else {
+                if (root.right == null) {
+                    return root.left;
+                } else if (root.left == null) {
+                    return root.right;
+                } else {
+                    Node temp = root;
+                    root = min(root);
+                    root.left = removeMin(temp.right);
+                    root.right = temp.right;
+                }
+
+            }
+        }
+        return root;
+    }
     /** Removes KEY from the tree if present
      *  returns VALUE removed,
      *  null on failed removal.
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException("call remove() with a null key");
+        } else {
+            V reVal = get(key);
+            root = removeHelper(key, root);
+            if (reVal != null) {
+                size -= 1;
+            }
+            return reVal;
+        }
     }
 
     /** Removes the key-value entry for the specified key only if it is
@@ -117,11 +182,35 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException("call remove() with a null key");
+        }
+        V reVal = get(key);
+        if (reVal != value) {
+            return null;
+        } else {
+            root = removeHelper(key, root);
+            size -= 1;
+            return reVal;
+        }
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return keySet().iterator();
+    }
+
+    public static void main(String[] args) {
+        BSTMap<String, Integer> bstmap = new BSTMap<>();
+        bstmap.put("hello", 5);
+        bstmap.put("cat", 10);
+        bstmap.put("fish", 22);
+        bstmap.put("zebra", 90);
+        int reVal = bstmap.remove("cat");
+        System.out.println(reVal);
+        System.out.println(bstmap.size());
+        for (String k: bstmap) {
+            System.out.println(k);
+        }
     }
 }
